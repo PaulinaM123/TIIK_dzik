71ec1b9f72f0cfb50e06b048f85c8965c9369539
Paulina Mrozek <paulina.mrozek123@gmail.com>
2018-10-23 13:25:52 +0200

    to do: bind table

---
+++ Lossless/Helpers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace Lossless
+{
+    public static class Helpers
+    {
+        static string DeleteNonChars(String value)
+        {
+            value = value.Replace("\n", String.Empty);
+            value = value.Replace("\r", String.Empty);
+            value = value.Replace("\t", String.Empty);
+            value = value.Replace(" ", String.Empty);
+
+            return value;
+        }
+
+        public static Dictionary<char, int> CountCharacters(string text)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            text = DeleteNonChars(text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (dict.ContainsKey(text[i]))
+                {
+                    dict[text[i]]++;
+                }
+                else
+                {
+                    dict.Add(text[i], 1);
+                }
+            }
+            return dict;
+        }
+
+        public static  void LoadDataToColumnSeries(DataPointSeries pointSeries, Dictionary<char, int> dictionary)
+        {
+            pointSeries.ItemsSource = dictionary.ToList().OrderBy(x => x.Key);
+        }
+
+        
+    }
+}
---
+++ Lossless/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace Lossless
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static String ReadingFromFile(String path)
+        {
+            string s = File.ReadAllText(path, Encoding.Default);
+            return s;
+        }
+
+        private void LoadText_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string path = dlg.FileName;
+                loaded_text.Text = ReadingFromFile(path);
+            }
+        }
+
+        static int CountNonSpaceChars(String value)
+        {
+            int result = 0;
+            foreach (char c in value)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+   
+        private void Statistics_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder builder = new StringBuilder(loaded_text.Text);
+            char_num.Text = CountNonSpaceChars(builder.ToString()).ToString();
+            var x = Helpers.CountCharacters(loaded_text.Text);
+            Helpers.LoadDataToColumnSeries((DataPointSeries)mcChart.Series[0], x);
+          
+        }
+    }
+}
